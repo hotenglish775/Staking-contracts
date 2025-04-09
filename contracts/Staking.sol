bcbd953bf9df582253fce421f8ffd1a88ceff5d7
hotenglish775 <support@acrypto.cloud>
2025-04-09 10:28:54 +0100

    Staking.sol

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,185 +1,111 @@
-pragma solidity ^0.8.7;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";         // Access control
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";   // Reentrancy protection
+import "@openzeppelin/contracts/security/Pausable.sol";          // Emergency pause
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";        // Safe arithmetic operations
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";           // ERC20 interface
 
-contract Staking {
-    using Address for address;
+contract SecureStaking is Ownable, ReentrancyGuard, Pausable {
+    using SafeMath for uint256;
 
-    // Parameters
-    uint128 public constant VALIDATOR_THRESHOLD = 20 ether;
+    IERC20 public stakingToken;         // ERC20 token used for staking (ACC-20)
+    uint256 public totalStaked;           // Total tokens staked
+    uint256 public rewardRate;            // Reward rate per block
+    uint256 public lastUpdateBlock;       // Last block when rewards were updated
+    uint256 public rewardPerTokenStored;  // Cumulative reward per token stored
+    uint256 public constant MIN_STAKE_AMOUNT = 1 * 10**18; // Minimum stake (e.g. 1 token)
 
-    // Properties
-    address[] public _validators;
+    struct Staker {
+        uint256 balance;              // Amount staked
+        uint256 rewardPerTokenPaid;   // Reward already paid
+        uint256 rewards;              // Accumulated rewards (pending claim)
+    }
 
-    mapping(address => bool) public _addressToIsValidator;
-    mapping(address => uint256) public _addressToStakedAmount;
-    mapping(address => uint256) public _addressToValidatorIndex;
-    uint256 public _stakedAmount;
-    uint256 public _minimumNumValidators;
-    uint256 public _maximumNumValidators;
+    mapping(address => Staker) public stakers;
 
-    mapping(address => bytes) public _addressToBLSPublicKey;
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
 
-    // Events
-    event Staked(address indexed account, uint256 amount);
+    constructor(address _stakingToken, uint256 _rewardRate) {
+        require(_stakingToken != address(0), "Invalid token address");
+        require(_rewardRate > 0, "Reward rate must be positive");
+        stakingToken = IERC20(_stakingToken);
+        rewardRate = _rewardRate;
+        lastUpdateBlock = block.number;
+    }
 
-    event Unstaked(address indexed account, uint256 amount);
-
-    event BLSPublicKeyRegistered(address indexed accout, bytes key);
-
-    // Modifiers
-    modifier onlyEOA() {
-        require(!msg.sender.isContract(), "Only EOA can call function");
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateBlock = block.number;
+        if (account != address(0)) {
+            stakers[account].rewards = earned(account);
+            stakers[account].rewardPerTokenPaid = rewardPerTokenStored;
+        }
         _;
     }
 
-    modifier onlyStaker() {
-        require(
-            _addressToStakedAmount[msg.sender] > 0,
-            "Only staker can call function"
-        );
-        _;
+    function setRewardRate(uint256 _rewardRate) external onlyOwner {
+        require(_rewardRate > 0, "Reward rate must be positive");
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateBlock = block.number;
+        rewardRate = _rewardRate;
     }
 
-    modifier onlyValidator() {
-        require(_isValidator(msg.sender), "Only validator can call function");
-        _;
+    function stake(uint256 amount) external nonReentrant whenNotPaused updateReward(msg.sender) {
+        require(amount >= MIN_STAKE_AMOUNT, "Amount below minimum stake");
+        totalStaked = totalStaked.add(amount);
+        stakers[msg.sender].balance = stakers[msg.sender].balance.add(amount);
+        require(stakingToken.transferFrom(msg.sender, address(this), amount), "Token transfer failed");
+        emit Staked(msg.sender, amount);
     }
 
-    constructor(uint256 minNumValidators, uint256 maxNumValidators) {
-        require(
-            minNumValidators <= maxNumValidators,
-            "Min validators num can not be greater than max num of validators"
-        );
-        _minimumNumValidators = minNumValidators;
-        _maximumNumValidators = maxNumValidators;
+    function withdraw(uint256 amount) public nonReentrant updateReward(msg.sender) {
+        require(amount > 0, "Cannot withdraw zero");
+        require(stakers[msg.sender].balance >= amount, "Insufficient balance");
+        totalStaked = totalStaked.sub(amount);
+        stakers[msg.sender].balance = stakers[msg.sender].balance.sub(amount);
+        require(stakingToken.transfer(msg.sender, amount), "Token transfer failed");
+        emit Withdrawn(msg.sender, amount);
     }
 
-    // View functions
-    function stakedAmount() public view returns (uint256) {
-        return _stakedAmount;
+    function exit() external {
+        withdraw(stakers[msg.sender].balance);
+        claimReward();
     }
 
-    function validators() public view returns (address[] memory) {
-        return _validators;
-    }
-
-    function validatorBLSPublicKeys() public view returns (bytes[] memory) {
-        bytes[] memory keys = new bytes[](_validators.length);
-
-        for (uint256 i = 0; i < _validators.length; i++) {
-            keys[i] = _addressToBLSPublicKey[_validators[i]];
+    function claimReward() public nonReentrant updateReward(msg.sender) {
+        uint256 reward = stakers[msg.sender].rewards;
+        if (reward > 0) {
+            stakers[msg.sender].rewards = 0;
+            require(stakingToken.transfer(msg.sender, reward), "Reward transfer failed");
+            emit RewardPaid(msg.sender, reward);
         }
-
-        return keys;
     }
 
-    function isValidator(address addr) public view returns (bool) {
-        return _addressToIsValidator[addr];
-    }
-
-    function accountStake(address addr) public view returns (uint256) {
-        return _addressToStakedAmount[addr];
-    }
-
-    function minimumNumValidators() public view returns (uint256) {
-        return _minimumNumValidators;
-    }
-
-    function maximumNumValidators() public view returns (uint256) {
-        return _maximumNumValidators;
-    }
-
-    // Public functions
-    receive() external payable onlyEOA {
-        _stake();
-    }
-
-    function stake() public payable onlyEOA {
-        _stake();
-    }
-
-    function unstake() public onlyEOA onlyStaker {
-        _unstake();
-    }
-
-    function registerBLSPublicKey(bytes memory blsPubKey) public {
-        _addressToBLSPublicKey[msg.sender] = blsPubKey;
-
-        emit BLSPublicKeyRegistered(msg.sender, blsPubKey);
-    }
-
-    // Private functions
-    function _stake() private {
-        _stakedAmount += msg.value;
-        _addressToStakedAmount[msg.sender] += msg.value;
-
-        if (_canBecomeValidator(msg.sender)) {
-            _appendToValidatorSet(msg.sender);
+    function rewardPerToken() public view returns (uint256) {
+        if (totalStaked == 0) {
+            return rewardPerTokenStored;
         }
-
-        emit Staked(msg.sender, msg.value);
-    }
-
-    function _unstake() private {
-        uint256 amount = _addressToStakedAmount[msg.sender];
-
-        _addressToStakedAmount[msg.sender] = 0;
-        _stakedAmount -= amount;
-
-        if (_isValidator(msg.sender)) {
-            _deleteFromValidators(msg.sender);
-        }
-
-        payable(msg.sender).transfer(amount);
-        emit Unstaked(msg.sender, amount);
-    }
-
-    function _deleteFromValidators(address staker) private {
-        require(
-            _validators.length > _minimumNumValidators,
-            "Validators can't be less than the minimum required validator num"
+        return rewardPerTokenStored.add(
+            block.number.sub(lastUpdateBlock).mul(rewardRate).mul(1e18).div(totalStaked)
         );
-
-        require(
-            _addressToValidatorIndex[staker] < _validators.length,
-            "index out of range"
-        );
-
-        // index of removed address
-        uint256 index = _addressToValidatorIndex[staker];
-        uint256 lastIndex = _validators.length - 1;
-
-        if (index != lastIndex) {
-            // exchange between the element and last to pop for delete
-            address lastAddr = _validators[lastIndex];
-            _validators[index] = lastAddr;
-            _addressToValidatorIndex[lastAddr] = index;
-        }
-
-        _addressToIsValidator[staker] = false;
-        _addressToValidatorIndex[staker] = 0;
-        _validators.pop();
     }
 
-    function _appendToValidatorSet(address newValidator) private {
-        require(
-            _validators.length < _maximumNumValidators,
-            "Validator set has reached full capacity"
-        );
-
-        _addressToIsValidator[newValidator] = true;
-        _addressToValidatorIndex[newValidator] = _validators.length;
-        _validators.push(newValidator);
+    function earned(address account) public view returns (uint256) {
+        return stakers[account].balance
+            .mul(rewardPerToken().sub(stakers[account].rewardPerTokenPaid))
+            .div(1e18)
+            .add(stakers[account].rewards);
     }
 
-    function _isValidator(address account) private view returns (bool) {
-        return _addressToIsValidator[account];
+    function pause() external onlyOwner {
+        _pause();
     }
 
-    function _canBecomeValidator(address account) private view returns (bool) {
-        return
-            !_isValidator(account) &&
-            _addressToStakedAmount[account] >= VALIDATOR_THRESHOLD;
+    function unpause() external onlyOwner {
+        _unpause();
     }
 }
